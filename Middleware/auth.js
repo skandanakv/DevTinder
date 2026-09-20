97078a19957552025198adcdbae6287639be5b57
Skandana KV <skandanakv@gmail.com>
2026-09-21 00:16:53 +0530

    updated ! token err

--- Middleware/auth.js
+++ Middleware/auth.js
@@ -6,7 +6,7 @@ const userAuth = async (req, res, next) => {
     const cookies=req.cookies;
     const {token}=cookies;
     if(!token){
-        throw new Error("Invalid Token");
+        res.status(401).send("Unauthorized, please login first");
     }
 
     const decodedObj=await jwt.verify(token, "Skandana@DevTinder");
